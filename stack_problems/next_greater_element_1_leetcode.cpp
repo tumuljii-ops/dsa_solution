4f6f5afd1a06c4e21efc4be49499dc1574eb0cd7
tumuljii-ops <tumuljii@gmail.com>
2026-01-08 19:03:11 +0530

    next greater element 1 problem leetcode

---
+++ stack_problems/next_greater_element_1_leetcode.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> st;
+        map<int,int> mpp;   
+        vector<int> result;
+
+        int n = nums2.size();
+        for(int i = n - 1; i >= 0; i--) {
+            while(!st.empty() && st.top() <= nums2[i]) {
+                st.pop();
+            }
+
+            if(st.empty()) {
+                mpp[nums2[i]] = -1;
+            } else {
+                mpp[nums2[i]] = st.top();
+            }
+
+            st.push(nums2[i]);
+        }
+        for(int i = 0; i < nums1.size(); i++) {
+            result.push_back(mpp[nums1[i]]);
+        }
+
+        return result;
+    }
+};

54188abf8e282f124a87bf69bddffa8e56ad2f61
tumuljii-ops <tumuljii@gmail.com>
2026-01-11 23:57:09 +0530

    4 sum problem leetcode

---
+++ array/4_sum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> array;
+        int n = nums.size();
+        if(n < 4) return array;
+
+        sort(nums.begin(), nums.end());
+
+        for(int i = 0; i < n-3; i++) {
+            if(i > 0 && nums[i] == nums[i-1]) continue; 
+
+            for(int j = i+1; j < n-2; j++) {
+                if(j > i+1 && nums[j] == nums[j-1]) continue;
+
+                int k = j+1;
+                int l = n-1;
+
+                while(k < l) {
+                    long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[l];
+
+                    if(sum < target) k++;
+                    else if(sum > target) l--;
+                    else {
+                        array.push_back({nums[i], nums[j], nums[k], nums[l]});
+                        while(k < l && nums[k] == nums[k+1]) k++;
+                        while(k < l && nums[l] == nums[l-1]) l--;
+
+                        k++;
+                        l--;
+                    }
+                }
+            }
+        }
+
+        return array;
+    }
+};

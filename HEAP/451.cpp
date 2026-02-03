00bfa16b97f92934fd23b9fdd2b6790147c6934f
tumuljii-ops <tumuljii@gmail.com>
2026-02-03 18:43:22 +0530

    sort char by freq

---
+++ HEAP/451.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+
+        unordered_map<char, int> freq;
+        for(char ch : s){
+            freq[ch]++;
+        }
+
+        priority_queue<pair<int, char>> pq;
+        for(auto it : freq){
+            pq.push({it.second, it.first});
+        }
+
+        string ans = "";
+        while(!pq.empty()){
+            auto top = pq.top();
+            pq.pop();
+
+            int count = top.first;
+            char ch = top.second;
+
+            while(count--){
+                ans += ch;
+            }
+        }
+
+        return ans;
+    }
+};

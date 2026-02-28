e0fb2dd7f488fe39931f9db0e7bc3f04a79e3d38
tumuljii-ops <tumuljii@gmail.com>
2026-02-28 23:30:33 +0530

    bitwise daily problem

---
+++ bitwise/1680_bitwise.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int concatenatedBinary(int n) {
+
+        const int MOD = 1000000007;
+        long long ans = 0;
+        int bits = 0;
+
+        for(int i = 1; i <= n; i++) {
+
+            if((i & (i - 1)) == 0)
+                bits++;
+
+            ans = ((ans << bits) + i) % MOD;
+        }
+
+        return (int)ans;
+    }
+};

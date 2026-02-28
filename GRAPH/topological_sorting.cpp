85463571f791e7d61d8e577abe52183a1acf3195
tumuljii-ops <tumuljii@gmail.com>
2026-02-28 23:38:42 +0530

    topological sort

---
+++ GRAPH/topological_sorting.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(V);
+        vector<int> indegree(V, 0);
+        for (auto &e : edges) {
+            int u = e[0];
+            int v = e[1];
+            adj[u].push_back(v);
+            indegree[v]++;
+        }
+
+        queue<int> q;
+        for (int i = 0; i < V; i++) {
+            if (indegree[i] == 0)
+                q.push(i);
+        }
+
+        vector<int> topo;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+
+            for (auto it : adj[node]) {
+                indegree[it]--;
+                if (indegree[it] == 0)
+                    q.push(it);
+            }
+        }
+
+        return topo;
+    }
+};

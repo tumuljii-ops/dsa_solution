b3b70e958fc77903460a2ef9ce50757ce368f906
tumuljii-ops <tumuljii@gmail.com>
2026-03-24 01:13:25 +0530

    number is even or odd

---
+++ bitwise/number_is_even_or_odd.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"enter number n:";
+    cin>>n;
+
+    if(n&1){
+        cout<<"odd"<<'\n';
+    }
+    else{
+        cout<<"even"<<'\n';
+    }
+}

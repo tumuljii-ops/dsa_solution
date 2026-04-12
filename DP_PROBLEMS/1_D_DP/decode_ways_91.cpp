7c63e532375da72343a50bc0c591620fb469755a
tumuljii-ops <tumuljii@gmail.com>
2026-04-12 17:10:14 +0530

    tabulation part

--- DP_PROBLEMS/1_D_DP/decode_ways_91.cpp
+++ DP_PROBLEMS/1_D_DP/decode_ways_91.cpp
@@ -32,3 +32,38 @@ public:
         return memo(dp, s, 0);
     }
 };
+
+// tabulation -----------------
+int numDecodings(string s) {
+       int n = s.length();
+
+        vector<int> dp(n + 1, 0);
+
+        dp[n] = 1;  
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            if (s[i] == '0') {
+                dp[i] = 0;
+                continue;
+            }
+
+            int left = dp[i + 1];
+
+            int right = 0;
+
+            if (i + 1 < n) {
+                int num = (s[i] - '0') * 10 + (s[i + 1] - '0');
+
+                if (num >= 10 && num <= 26) {
+                    right = dp[i + 2];
+                }
+            }
+
+            dp[i] = left + right;
+        }
+
+        return dp[0];
+    }
+};
+

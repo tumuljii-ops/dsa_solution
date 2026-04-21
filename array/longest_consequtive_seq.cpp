1c60be9a7fa909f0a7c7f4e43bc6eeb1e4579cb5
tumuljii-ops <tumuljii@gmail.com>
2026-04-21 11:53:41 +0530

    longest consequtive sequenece

---
+++ array/longest_consequtive_seq.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        
+        unordered_set<int> st(nums.begin(), nums.end());
+        int longest = 0;
+
+        for(int num : st){
+            
+            if(st.find(num - 1) == st.end()){
+                
+                int current = num;
+                int length = 1;
+
+                while(st.find(current + 1) != st.end()){
+                    current++;
+                    length++;
+                }
+
+                longest = max(longest, length);
+            }
+        }
+
+        return longest;
+    }
+};

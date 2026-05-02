f75155fc8c1eb053823fffaf07677897c0fe0730
tumuljii-ops <tumuljii@gmail.com>
2026-05-02 15:43:15 +0530

    alien dictonary

--- GRAPH/alien_dictonary.cpp
+++ GRAPH/alien_dictonary.cpp
@@ -1,55 +1,76 @@
-   vector<vector<int>> adj(26);
+class Solution {
+public:
+    string findOrder(vector<string> &words) {
+        
+        vector<vector<int>> adj(26);
         vector<int> indegree(26, 0);
-        vector<int> present(26, 0);
+        vector<bool> present(26, false);
+        
+        int n = words.size();
 
-        for (auto &w : words)
-            for (char c : w)
-                present[c - 'a'] = 1;
-
-        // Build graph + prefix invalid check
-        for (int i = 0; i < (int)words.size() - 1; i++) {
-            string &a = words[i];
-            string &b = words[i + 1];
-
-            int len = min(a.size(), b.size());
-            int j = 0;
-
-            while (j < len && a[j] == b[j]) j++;
-
-            // if b is prefix of a => invalid
-            if (j == len && a.size() > b.size()) return "";
-
-            // first mismatch gives edge
-            if (j < len) {
-                int u = a[j] - 'a';
-                int v = b[j] - 'a';
-                adj[u].push_back(v);
-                indegree[v]++;
+      
+        for(auto word : words){
+            for(auto ch : word){
+                present[ch - 'a'] = true;
             }
         }
 
-        // Topo sort (Kahn)
+    
+        for(int i = 1; i < n; i++){
+            string s1 = words[i - 1];
+            string s2 = words[i];
+
+            int p = 0, q = 0;
+
+            while(p < s1.length() && q < s2.length()){
+                if(s1[p] != s2[q]){
+                    adj[s1[p] - 'a'].push_back(s2[q] - 'a');
+                    indegree[s2[q] - 'a']++;
+                    break;
+                }
+                p++;
+                q++;
+            }
+
+            if(p == s2.length() && s1.length() > s2.length()){
+                return "";
+            }
+        }
+
         queue<int> q;
-        int total = 0;
-
-        for (int i = 0; i < 26; i++) {
-            if (present[i]) {
-                total++;
-                if (indegree[i] == 0) q.push(i);
+        for(int i = 0; i < 26; i++){
+            if(present[i] && indegree[i] == 0){
+                q.push(i);
             }
         }
 
-        string ans = "";
-        while (!q.empty()) {
-            int node = q.front(); q.pop();
-            ans.push_back(char('a' + node));
+        string result = "";
 
-            for (int nei : adj[node]) {
-                indegree[nei]--;
-                if (indegree[nei] == 0) q.push(nei);
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+
+            result += (node + 'a');
+
+            for(auto it : adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0){
+                    q.push(it);
+                }
             }
         }
 
-        // Cycle check
-        if ((int)ans.size() != total) return "";
-        return ans;
+ 
+        int count = 0;
+        for(int i = 0; i < 26; i++){
+            if(present[i]) count++;
+        }
+
+        if(result.size() != count){
+            return "";
+        }
+
+        return result;
+    }
+};
+

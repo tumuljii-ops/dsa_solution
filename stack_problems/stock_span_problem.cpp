823dc49b83549959d2250d02e3a141aa64c534c4
tumuljii-ops <tumuljii@gmail.com>
2026-05-03 18:39:24 +0530

    stock span problem solved

--- stack_problems/stock_span_problem.cpp
+++ stack_problems/stock_span_problem.cpp
@@ -1,18 +1,36 @@
-class StockSpanner {
-    stack<pair<int,int>>st;
+class Solution {
 public:
-    StockSpanner() {
+    vector<int> calculateSpan(vector<int>& arr) {
         
-    }
-    
-    int next(int price) {
-        int span=1;
-
-        while(!st.empty() && st.top().first <=price){
-            span=span+st.top().second;
-            st.pop();
+        int n = arr.size();
+        
+        vector<int> ans(n, 0);
+        stack<int> st;
+        
+        st.push(0);
+        ans[0] = -1;   
+        
+        for (int i = 1; i < n; i++) {
+            
+            while (!st.empty() && arr[st.top()] <= arr[i]) {
+                st.pop();
+            }
+            
+            if (st.empty()) {
+                ans[i] = -1;  
+            } else {
+                ans[i] = st.top();
+            }
+            
+            st.push(i);
         }
-        st.push({price,span});
-        return span;
+        
+        vector<int> ret(n);
+        
+        for (int i = 0; i < n; i++) {
+            ret[i] = i - ans[i];
+        }
+        
+        return ret;
     }
 };

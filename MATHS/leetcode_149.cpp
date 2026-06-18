4cc121d0756e1c6b12e9da309e4746b8736e175d
TUMUL SINGH <tumuljii@gmail.com>
2026-06-18 13:04:57 +0530

    today problem done

---
+++ MATHS/leetcode_149.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& points) {
+
+        int n = points.size();
+
+        if(n <= 2) return n;
+
+        int ans = 2;
+
+        for(int i = 0; i < n; i++) {
+
+            map<pair<int,int>, int> mp;
+
+            for(int j = i + 1; j < n; j++) {
+
+                int dx = points[j][0] - points[i][0];
+                int dy = points[j][1] - points[i][1];
+
+                // Vertical line
+                if(dx == 0) {
+                    dy = 1;
+                }
+                // Horizontal line
+                else if(dy == 0) {
+                    dx = 1;
+                }
+                else {
+
+                    int g = gcd(abs(dx), abs(dy));
+
+                    dx /= g;
+                    dy /= g;
+
+                    // Normalize sign
+                    if(dx < 0) {
+                        dx = -dx;
+                        dy = -dy;
+                    }
+                }
+
+                mp[{dx, dy}]++;
+
+                ans = max(ans, mp[{dx, dy}] + 1);
+            }
+        }
+
+        return ans;
+    }
+};

97900cfd71976f67ae870ed48abf5b276c46ed43
TUMUL SINGH <tumuljii@gmail.com>
2026-08-17 01:09:09 +0530

    maths problem dsa

--- MATHS/166.cpp
+++ MATHS/166.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     string fractionToDecimal(int numerator, int denominator) {
-          
-            if(numerator==0) return "0";
+           
+            if(numerator==0){
+                return "0";
+            }
 
             string ans="";
 
-            if((numerator<0 && denominator>0) && (numerator>0 && denominator<0)){
-                  ans+="-";
+            if((numerator>0 && denominator<0)||(numerator<0 && denominator>0)){
+                 ans+="-";
             }
 
             long long num=abs((long long)numerator);
@@ -15,29 +17,34 @@ public:
 
             ans+=to_string(num/den);
 
-            if(num/den==0){
-                 return ans;
+            long long rem=num%den;
+
+            if(rem==0){
+                return ans;
             }
 
-           long long rem=num%den;
+            ans+=".";
 
-            unordered_map<long long ,int>mpp;
+            unordered_map<long long,int>mpp;
 
             while(rem!=0){
+                  
+                  if(mpp.find(rem)!=mpp.end()){
+                     
+                        int pos=mpp[rem];
 
-                if(mpp(rem)!=mpp.end()){
-                     int pos=mpp[rem];
+                        ans.insert(pos,"(");
+                        ans+=")";
+                        break;
+                  }
 
-                     ans.insert(pos,"(");
-                     ans+=")";
-                     break;
-                }
-               
-                mpp[rem]=ans.length();
+                  mpp[rem]=ans.length();
 
-                rem=rem*10;
-                ans+=to_string(rem/den);
-                rem=rem%den;
+                  rem=rem*10;
+                  ans=ans+to_string(rem/den);
+
+                  rem=rem%den;
+
             }
 
             return ans;

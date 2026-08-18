1d6f3c2d3f281d68de9fb3682421d9fdb8595575
TUMUL SINGH <tumuljii@gmail.com>
2026-08-18 16:32:40 +0530

    another solution for problem 71

--- stack_problems/71.cpp
+++ stack_problems/71.cpp
@@ -1,74 +1,38 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        stack<char> st;
-        int n = path.length();
+            
+             string token="";
+             stringstream ss(path);
+             stack<string>st;
 
-        st.push('/');
-
-        for (int i = 1; i < n; i++) {
-
-            if (path[i] == '/' && st.top() == '/') continue;
-
-            if (path[i] == '.') {
-
-                if (st.top() != '/') {
-                    st.push('.');
-                    continue;
-                }
-
-                int dotCount = 0;
-
-                while (i < n && path[i] == '.') {
-                    dotCount++;
-                    i++;
-                }
-
-                if (i == n || path[i] == '/') {
-
-                    if (dotCount == 1) {
-                        i--;
+             while(getline(ss,token,'/')){
+                 
+                   if(token=="." || token==""){
                         continue;
-                    }
-                    else if (dotCount == 2) {
-                        if (st.size() > 1) {
-                            st.pop();
-                            while (st.size() > 1 && st.top() != '/') {
-                                st.pop();
-                            }
-                        }
-                        i--;
-                        continue;
-                    }
-                    else {
-                        while (dotCount--) st.push('.');
-                    }
-                }
-                else {
-                    while (dotCount--) st.push('.');
-                }
+                   }
 
-                i--;
-                continue;
-            }
+                   if(token!=".."){
+                      st.push(token);
+                   }
+                   else if(!st.empty()){
+                       st.pop();
+                   }
+             }
 
-            if (path[i] == '/' && st.top() == '/') continue;
-            st.push(path[i]);
-        }
+             if(st.empty()){
+                   return "/";
+             }
 
-        string ans;
-        while (!st.empty()) {
-            ans += st.top();
-            st.pop();
-        }
+             string result=""; 
 
-        reverse(ans.begin(), ans.end());
+             while(!st.empty()){
+                 
+                 result="/"+st.top()+result;
+                 st.pop();
+             }
 
-        if (ans.size() > 1 && ans.back() == '/') {
-            ans.pop_back();
-        }
-
-        return ans;
+             return result;
     }
 };
 

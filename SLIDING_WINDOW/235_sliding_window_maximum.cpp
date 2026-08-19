3a4d658c07c7e03e5853ad0991f5f079971fc3b7
TUMUL SINGH <tumuljii@gmail.com>
2026-08-19 13:20:10 +0530

    dequeue based question

--- SLIDING_WINDOW/235_sliding_window_maximum.cpp
+++ SLIDING_WINDOW/235_sliding_window_maximum.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        
-        // Deque will store indices
-        deque<int> dq;
+           
+           vector<int>ans;
+           deque<int>dq;
 
-        vector<int> ans;
+           for(int i=0;i<nums.size();i++){
+             
+                  while(!dq.empty() && dq.front()<=i-k){
+                       dq.pop_front();
+                  }
 
-        int n = nums.size();
+                  while(!dq.empty() && nums[dq.back()]<nums[i]){
+                      dq.pop_back();
+                  }
 
-        for(int i = 0; i < n; i++){
+                  dq.push_back(i);
 
-            // Remove indices that are out of current window
-            // Window range = [i-k+1 , i]
-            while(!dq.empty() && dq.front() <= i - k){
-                dq.pop_front();
-            }
+                  if(i>=k-1){
+                     ans.push_back(nums[dq.front()]);
 
-            // Remove smaller elements from back
-            // because they can never become maximum
-            while(!dq.empty() && nums[dq.back()] < nums[i]){
-                dq.pop_back();
-            }
+                  }
+           }
 
-            // Push current index
-            dq.push_back(i);
+           return ans;
 
-            // Start storing answers when first window is formed
-            if(i >= k - 1){
-
-                // Front of deque contains maximum element index
-                ans.push_back(nums[dq.front()]);
-            }
-        }
-
-        return ans;
     }
 };

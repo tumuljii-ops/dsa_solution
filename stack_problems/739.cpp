a9d0b9ad8be06b13d90a5dc39b574dccb9f82899
TUMUL SINGH <tumuljii@gmail.com>
2026-08-25 23:08:39 +0530

    stack problem

---
+++ stack_problems/739.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+                
+                int n=temperatures.size();
+                stack<pair<int,int>>st;
+
+                vector<int>ans(n);
+
+                for(int i=n-1;i>=0;i--){
+                       
+                      while(!st.empty() && st.top().first<=temperatures[i]){
+                             st.pop();
+                      }
+
+                      if(st.empty()){
+                         ans[i]=0;
+                      }
+                      else{
+                          ans[i]=st.top().second-i;
+                      }
+
+                      st.push({temperatures[i],i});
+
+
+                }
+
+                return ans;
+    }
+};
